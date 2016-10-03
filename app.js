5fc38f6e9f7ff6e7ab51331ebe518914a39caa55
dlucas25 <danielmartinez@MacBook-Pro-de-Daniel.local>
2016-10-03 01:56:55 -0500

    api

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var usuarios = require('./routes/usuarios');
+var codigos = require('./routes/codigos');
+var promociones = require('./routes/promociones');
+
 
 var app = express();
 
+mongoose.connect("mongodb://localhost/realidad");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,6 +30,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/usuarios',usuarios);
+app.use('/codigos', codigos);
+app.use('/promociones', promociones);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
